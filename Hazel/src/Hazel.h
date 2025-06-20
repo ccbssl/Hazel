01af367c09e476aeb64a240147381dcb450ee1c7
ccbssl <1936668009@qq.com>
2025-06-20 14:49:44 +0800

    premake

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,11 @@
+#pragma once
+
+//for use by hazel application
+
+#include"Hazel/Application.h"
+#include"Hazel/Log.h"
+
+//-----Entry Point-------------
+#include"Hazel/EntryPoint.h"
+//-----------------------------
+
