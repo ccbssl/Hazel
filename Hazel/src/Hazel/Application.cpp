718c76dceaec18d3bb2a1bb75846c2c420c03c3b
ccbssl <1936668009@qq.com>
2025-06-25 20:52:15 +0800

    pink

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -3,6 +3,7 @@
 #include "Application.h"
 
 #include"Hazel/Events/ApplicationEvent.h"
+#include<GLFW/glfw3.h>
 
 namespace Hazel {
 
@@ -19,6 +20,8 @@ namespace Hazel {
 	{
 		while (m_Running)
 		{
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
 	}
